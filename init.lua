4d14f50b7d2ece5303b88a804adc692fedfdbb14
David Thompson <david@the-thompsons.com>
2025-06-15 06:35:11 +0100

    added black and isort

--- init.lua
+++ init.lua
@@ -631,6 +631,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'black',
+        'isort',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -688,6 +690,11 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        isort = {
+          prepend_args = { '--float-to-top' },
+        },
       },
     },
   },
